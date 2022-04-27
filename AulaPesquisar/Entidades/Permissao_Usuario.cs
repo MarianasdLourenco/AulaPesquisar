d80a4e1b583d0f983132074de92620af8f021759
MARIANA LOURENCO <mariana.lourenco@fatec.sp.gov.br>
2022-04-27 10:57:06 -0300

    aula

---
+++ AulaPesquisar/Entidades/Permissao_Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPesquisar.Entidades
+{
+    public class Permissao_Usuario
+    {
+
+        public int Id { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public int PermissaoId { get; set; }
+
+        public Permissao Permissao { get; set; } // ligaçãõ virtual
+
+        public Usuarios Usuario { get; set; } // ligaçãõ virtual
+    }
+}
